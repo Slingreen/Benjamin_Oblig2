feb202326dc9fb8a665eea6a6af56bcf571ef1bd
Slingreen <benjamindarici@hotmail.com>
2020-10-13 21:28:49 +0200

    Add project files.

---
+++ Benjamin_Oblig2/Benjamin_Oblig2.cpp
@@ -0,0 +1,308 @@
+/**********************************************************************************************************
+ *      2nd obligatory assignment, BlackJack
+ *
+ *      A program that allows you to BlackJack against the "house"
+ *
+ *  @file    Benjamin_Oblig2.CPP
+ *  @author  Benjamin A.E. Darici
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <conio.h>
+#include <Windows.h>
+
+struct card {
+    int value = 0;
+    std::string name = "";
+    std::string suit = "";
+};
+
+struct person {
+    int cardValue = 0,
+        money = 100;
+    std::vector<card*> hand;
+};
+
+//  A function that returns numbers within a min and max value
+int betting(const char* c, int p, int h) {
+    char input[200] = "";
+    int  number = 0;
+    bool wrong = false;
+    int min = 0;
+    int max;
+    if(p>h)
+    {
+        max = h;
+    }
+    else 
+    {
+        max = p;
+    }
+
+    do {
+        wrong = false;
+        std::cout << c << " (" << min << " - " << max << "):  ";
+        std::cin.getline(input, 200);
+        number = atoi(input);
+        if (number == 0 && input[0] != '0')
+        {
+            wrong = true;   std::cout << "\nNot a number\n\n";
+        }
+    } while (wrong || number < min || number > max);
+
+    return number;
+}
+
+std::vector<card*> makeDeck() 
+{
+    std::vector<card*> newDeck;
+    std::vector<std::string> cardName{ "Jack", "Queen", "King" };
+    std::vector<std::string> cardSuit{ "Heart", "Spade", "Diamond", "Club"};
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            newDeck.push_back(new card);
+            newDeck[j+13*i]->value = j+1;
+            newDeck[j+13*i]->suit = cardSuit[i];
+            newDeck[j + 13 * i]->name = std::to_string(j+1);
+            if (j == 0)
+            {
+                newDeck[j + 13 * i]->name = "Ace";
+            }
+
+            if ((j+1)%10==0)
+            {
+                for (int k = 1; k <= 3; k++)
+                {
+                    newDeck.push_back(new card);
+                    newDeck[j+k+13*i]->value = j+1;
+                    newDeck[j + k + 13 * i]->suit = cardSuit[i];
+                    newDeck[j + k + 13 * i]->name = cardName[k-1];
+                }
+            }
+        }
+    }
+    return newDeck;
+}
+
+void shuffleDeck(std::vector<card*>& deck, std::vector<card*>& ph, std::vector<card*>& hh) 
+{
+    for (int i = 0; i < ph.size(); i++)
+    {
+        deck.push_back(ph[i]);
+    }
+    ph.clear();
+    for (int i = 0; i < hh.size(); i++)
+    {
+        deck.push_back(hh[i]);
+    }
+    hh.clear();
+    std::srand(static_cast<unsigned int>(std::time(nullptr))); // set initial seed value to system clock
+    std::random_shuffle(deck.begin(), deck.end());
+}
+
+//  Drawing cards from the deck
+void draw(std::vector<card*>& h, std::vector<card*>& d )
+{
+    std::srand(static_cast<unsigned int>(std::time(nullptr))); // set initial seed value to system clock
+    int draw = std::rand() % d.size();
+    h.push_back(d[draw]);
+    d[draw] = d[d.size()-1];
+    d.pop_back();
+}
+
+void renderCards(const std::vector<card*>& h) {
+    for (int i = 0; i < h.size(); i++)
+    {
+        std::cout << " ____________  ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < h.size(); i++)
+    {
+        std::cout << " |  " << std::right << std::setw(8) << h[i]->suit << "|  ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < h.size(); i++)
+    {
+        std::cout << " |" << std::left << std::setw(5) << h[i]->name << "     |  ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int i = 0; i < h.size(); i++)
+        {
+            std::cout << " |          |  ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+//  The game loop
+void game(std::vector<card*>& deck, person& player, person& house, int p)
+{
+    player.cardValue = house.cardValue = 0;
+    int bet{ 0 };
+    char input{' '};
+
+    draw(house.hand, deck);
+    if (house.hand.back()->value == 1)
+    {
+        if (house.cardValue < 11) {
+            house.cardValue += 11;
+        }
+        else
+        {
+            house.cardValue += 1;
+        }
+    }
+    else
+    {
+        house.cardValue += house.hand.back()->value;
+    }
+
+    do
+    {
+        system("CLS");
+
+        std::cout << "House funds: $" << house.money << std::endl;
+        renderCards(house.hand);
+        std::cout << std::endl << std::endl << "Prize pool: $" << p << std::endl << std::endl;
+
+        draw(player.hand, deck);
+        renderCards(player.hand);
+        std::cout << std::endl << "Player funds: $" << player.money << std::endl;
+
+        if (player.hand.back()->value == 1)
+        {
+            std::cout << "Make it 11? Y/n" << std::endl;
+            input = _getch();
+            if (toupper(input) == 'Y')
+            {
+                player.cardValue += 11;
+            }
+            else
+            {
+                player.cardValue += 1;
+            }
+        }
+        else
+        {
+            player.cardValue += player.hand.back()->value;
+        }
+
+        if (player.cardValue > 20) 
+        {
+            break;
+        }
+
+        std::cout << "[H]old?" << std::endl;
+        input = _getch();      
+    } while (toupper(input)!= 'H' && player.cardValue < 21);
+
+    if (player.cardValue > 21)
+    {   
+        std::cout << "Bust!" << std::endl;
+        house.money += p;
+        return;
+    }
+
+    //std::cin.ignore(200, '\n');
+    bet = betting("How much do you wish to bet?", player.money, house.money);
+    player.money -= bet;
+    house.money -= bet;
+    p += bet * 2;
+
+    do
+    {
+        Sleep(2000);
+        system("CLS");
+        draw(house.hand, deck);
+        if (house.hand.back()->value == 1)
+        {
+            if (house.cardValue < 11) {
+                house.cardValue += 11;
+            }
+            else
+            {
+                house.cardValue += 1;
+            }
+        }
+        else
+        {
+            house.cardValue += house.hand.back()->value;
+        }
+
+        std::cout << "House funds: $" << house.money << std::endl;
+        renderCards(house.hand);
+        std::cout << std::endl << std::endl << "Prize pool: $" << p << std::endl << std::endl;
+        renderCards(player.hand);
+        std::cout << std::endl << "Player funds: $" << player.money << std::endl;
+        
+    } while (house.cardValue < player.cardValue);
+
+    if (house.cardValue < player.cardValue || house.cardValue > 21) 
+    {
+        std::cout << "Player wins!" << std::endl;
+        player.money += p;
+        return;
+    }
+    else if (house.cardValue > player.cardValue) 
+    {
+        std::cout << "House wins!" << std::endl;
+        house.money += p;
+        return;
+    }
+    else {
+        std::cout << "It's a tie! Play again." << std::endl;
+        shuffleDeck(deck, player.hand, house.hand);
+        game(deck, player, house, p);
+    }
+}
+/*
+     __
+  -=(o '.    What spaghetti nonesne is this
+     '.-.\
+     /|  \\
+     '|  ||
+      _\_):,_
+*/
+
+
+
+//  Main function with menu
+int main()
+{
+    int prizePool{ 20 };
+    char command{ ' ' };
+    person player;
+    person house;
+    std::vector<card*> deck = makeDeck();
+    std::srand(static_cast<unsigned int>(std::time(nullptr))); // set initial seed value to system clock
+    std::random_shuffle(deck.begin(), deck.end());
+
+    while (toupper(command) != 'N' && player.money > 10 && house.money > 10)
+    {
+        player.money -= 10;
+        house.money -= 10;
+        game(deck, player, house, prizePool);
+        if (house.money < 10) {
+            std::cout << "\nCan't play anymore, you robbed the house!";
+            break;
+        }
+        else if (player.money < 10) {
+            std::cout << "\nCan't play anymore, you are broke!";
+            break;
+        }
+        std::cout << "Play another round of BlackJack? 10$ entry fee. y/N" << std::endl;
+        command = _getch();
+        shuffleDeck(deck, player.hand, house.hand);
+    }
+    std::cout << "\nWelcome back again!\n\n";
+}
